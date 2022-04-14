01b43ff113acbfe383a5d7980bcf4b0c72b86efe
PriCepThyk <74743228+PriCepThyk@users.noreply.github.com>
2022-04-14 04:31:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+	  function func() {
+	    var result;
+	    var num1 = Number(document.getElementById("num1").value);
+	    var num2 = Number(document.getElementById("num2").value);
+	    result = num1 + num2;
+	    console.log((result));
+	  }
